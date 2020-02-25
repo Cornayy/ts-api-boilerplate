63eda4f991afc0f518c5117c7fc708120e1b2753
Cornayy <38261196+Cornayy@users.noreply.github.com>
2020-02-26 00:10:51 +0100

    refactor: add books to api baseurl

--- src/tests/author.spec.ts
+++ src/tests/author.spec.ts
@@ -1,6 +1,7 @@
 import { agent as request } from 'supertest';
 import { describe, it } from 'mocha';
 import { expect } from 'chai';
+import { apiOptions } from '../config/options';
 import { Container } from 'typedi';
 import { App } from '../../src/app';
 
@@ -9,7 +10,7 @@ const { app } = Container.get(App);
 describe('Testing authors route', () => {
     describe('index route', () => {
         it('Should return all authors', async () => {
-            const { body } = await request(app).get('/api/v1/authors');
+            const { body } = await request(app).get(`${apiOptions.baseUrl}/authors`);
             const firstAuthor = body[0];
 
             expect(body).to.be.an('array');
--- src/tests/book.spec.ts
+++ src/tests/book.spec.ts
@@ -1,6 +1,7 @@
 import { agent as request } from 'supertest';
 import { describe, it } from 'mocha';
 import { expect } from 'chai';
+import { apiOptions } from '../config/options';
 import { Container } from 'typedi';
 import { App } from '../../src/app';
 
@@ -9,7 +10,7 @@ const { app } = Container.get(App);
 describe('Testing books route', () => {
     describe('index route', () => {
         it('Should return all books', async () => {
-            const { body } = await request(app).get('/api/v1/books');
+            const { body } = await request(app).get(`${apiOptions.baseUrl}/books`);
             const firstBook = body[0];
 
             expect(body).to.be.an('array');
